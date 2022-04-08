62859e70b474a824da823a91c9e71e5b12fc9e45
Joey Cos <joeycosentino24@gmail.com>
2022-04-08 14:14:43 -0400

    added to JS file, worked more with APIs

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,20 +9,26 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
-var inputValue = document.querySelector("#cityname");
+
 var submitBtn = document.querySelector("#submit");
 var apiKey = "139bfa69532b5f8c4c783dac503cee31";
 
 
-var getCityWeather = function(city) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?=" + city + "&appid=" + apiKey;
+var getCoords = function(event) {
+    var city = document.querySelector("input[id='cityname']").value;
+
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    console.log(apiUrl);
     // make a request to the url
     fetch(apiUrl)
         .then(function(response) {
             // request was successful
             if (response.ok) {
                 response.json().then(function(data) {
-                    displayCity(city);
+                    var lon = data.coord.lon;
+                    var lat = data.coord.lat;
+                    getCityWeather(lon, lat);
+                    document.querySelector("input[id='cityname']").value = "";
                 });
             } else {
                 alert("City Not Found");
@@ -33,6 +39,26 @@ var getCityWeather = function(city) {
         });
 };
 
-console.log(apiUrl);
+var getCityWeather = function (lon, lat) {
+    var apiUrl2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=" + apiKey;
+    console.log(apiUrl2);
 
-submitBtn.addEventListener("submit", getCityWeather);
+    // make a request again using what we got from previous fetch
+    fetch(apiUrl2)
+        .then(function(response) {
+            //request was successful
+            if (response.ok) {
+                response.json().then(function(data) {
+                    displayWeather(data);
+                    console.log(data);
+                })
+            }
+        })
+}
+
+var displayWeather = function (data) {
+    var currentDay = new Date(data.current.dt * 1000);
+    console.log(currentDay);
+}
+
+submitBtn.addEventListener("click", getCoords);
